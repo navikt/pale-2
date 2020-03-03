1b9deb88c18f2a7a8bc74cc51b46d72a21041740
Joakim Kartveit <joakimkartveit@gmail.com>
2020-03-03 13:22:32 +0100

    Legger til riktig apprec konstant

--- src/main/kotlin/no/nav/syfo/model/PaleConstant.kt
+++ src/main/kotlin/no/nav/syfo/model/PaleConstant.kt
@@ -1,7 +1,7 @@
 package no.nav.syfo.model
 
 enum class PaleConstant(val string: String) {
-    apprec("apprec"),
+    apprec("APPREC"),
     apprecVersionV1_0("1.0 2004-11-21"),
     ebRoleNav("Nav"),
     ebServiceLegemelding("Legemelding"),
