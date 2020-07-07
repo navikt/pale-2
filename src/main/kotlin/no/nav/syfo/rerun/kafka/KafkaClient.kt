4df0cea026e7666b4521f6181893b300d57ecf57
Tia Firing <tia.firing@nav.no>
2020-07-07 15:52:14 +0200

    leser fra kafka som json

--- src/main/kotlin/no/nav/syfo/rerun/kafka/KafkaClient.kt
+++ src/main/kotlin/no/nav/syfo/rerun/kafka/KafkaClient.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.rerun.kafka
 
+import no.nav.helse.eiFellesformat.XMLEIFellesformat
 import no.nav.syfo.Environment
 import no.nav.syfo.VaultSecrets
 import no.nav.syfo.kafka.envOverrides
@@ -9,12 +10,12 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.serialization.StringDeserializer
 
-fun getKafkaRerunConsumer(env: Environment, vaultSecrets: VaultSecrets): KafkaConsumer<String, String> {
+fun getKafkaRerunConsumer(env: Environment, vaultSecrets: VaultSecrets): KafkaConsumer<String, XMLEIFellesformat> {
     val kafkaBaseConfig = loadBaseConfig(env, vaultSecrets).envOverrides()
-    val properties = kafkaBaseConfig.toConsumerConfig("${env.applicationName}-consumer", valueDeserializer = StringDeserializer::class)
+    val properties = kafkaBaseConfig.toConsumerConfig("${env.applicationName}-consumer", valueDeserializer = JacksonKafkaDeserializer::class)
     properties.let { it[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "1" }
 
-    val kafkaRerunConsumer = KafkaConsumer<String, String>(properties)
+    val kafkaRerunConsumer = KafkaConsumer<String, XMLEIFellesformat>(properties, StringDeserializer(), JacksonKafkaDeserializer(XMLEIFellesformat::class))
     kafkaRerunConsumer.subscribe(listOf(env.pale2DumpTopic))
     return kafkaRerunConsumer
 }
--- src/main/kotlin/no/nav/syfo/rerun/kafka/RerunConsumer.kt
+++ src/main/kotlin/no/nav/syfo/rerun/kafka/RerunConsumer.kt
@@ -1,12 +1,13 @@
 package no.nav.syfo.rerun.kafka
 
 import java.time.Duration
+import no.nav.helse.eiFellesformat.XMLEIFellesformat
 import org.apache.kafka.clients.consumer.KafkaConsumer
 
 class RerunConsumer(
-    private val kafkaRerunConsumer: KafkaConsumer<String, String>
+    private val kafkaRerunConsumer: KafkaConsumer<String, XMLEIFellesformat>
 ) {
-    fun poll(): List<String> {
+    fun poll(): List<XMLEIFellesformat> {
         return kafkaRerunConsumer.poll(Duration.ofMillis(0)).map { it.value() }
     }
 }
