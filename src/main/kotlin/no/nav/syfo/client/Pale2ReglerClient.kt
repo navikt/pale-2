f017acd2228714bc84b587001689d54880511dd7
Tia Firing <tia.firing@nav.no>
2022-04-26 11:43:52 +0200

    oppgraderer ktor

--- src/main/kotlin/no/nav/syfo/client/Pale2ReglerClient.kt
+++ src/main/kotlin/no/nav/syfo/client/Pale2ReglerClient.kt
@@ -1,20 +1,21 @@
 package no.nav.syfo.client
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.accept
 import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
-import no.nav.syfo.helpers.retry
 import no.nav.syfo.model.ReceivedLegeerklaering
 import no.nav.syfo.model.ValidationResult
 
 class Pale2ReglerClient(private val endpointUrl: String, private val client: HttpClient) {
-    suspend fun executeRuleValidation(payload: ReceivedLegeerklaering): ValidationResult = retry("pale2regler_validate") {
-        client.post<ValidationResult>("$endpointUrl/v1/rules/validate") {
+    suspend fun executeRuleValidation(payload: ReceivedLegeerklaering): ValidationResult {
+        return client.post("$endpointUrl/v1/rules/validate") {
             contentType(ContentType.Application.Json)
             accept(ContentType.Application.Json)
-            body = payload
-        }
+            setBody(payload)
+        }.body<ValidationResult>()
     }
 }
