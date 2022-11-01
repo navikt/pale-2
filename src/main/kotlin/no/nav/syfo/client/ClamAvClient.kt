5e9e3d83e6824d071e8d0e8ff5bd102a1646c1b2
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-11-01 13:32:02 +0100

    Dropping checking result.size

--- src/main/kotlin/no/nav/syfo/client/ClamAvClient.kt
+++ src/main/kotlin/no/nav/syfo/client/ClamAvClient.kt
@@ -6,7 +6,6 @@ import io.ktor.client.request.forms.formData
 import io.ktor.client.request.forms.submitFormWithBinaryData
 import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
-import no.nav.syfo.log
 import no.nav.syfo.vedlegg.model.Vedlegg
 import java.util.Base64
 
@@ -21,21 +20,16 @@ class ClamAvClient(
                 formData = formData {
                     vedleggList.forEachIndexed { index, vedlegg ->
                         append(
-                            vedlegg.description + vedlegg.type, Base64.getMimeDecoder().decode(vedlegg.content.content),
+                            "file$index", Base64.getMimeDecoder().decode(vedlegg.content.content),
                             Headers.build {
                                 append(HttpHeaders.ContentType, vedlegg.content.contentType)
-                                append(HttpHeaders.ContentDisposition, "file" + index + "=${vedlegg.description + vedlegg.type}")
+                                append(HttpHeaders.ContentDisposition, "filename=${vedlegg.description}")
                             }
                         )
                     }
                 }
             )
-        val result = httpResponse.body<List<ScanResult>>()
-        if (result.size != 1) {
-            log.warn("result size: " + result.size)
-            throw RuntimeException("Unexpected result size from virus scan request")
-        }
-        return result
+        return httpResponse.body<List<ScanResult>>()
     }
 }
 
