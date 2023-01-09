d704e4236b11592292160752960ffa2fbe8cd617
oysteinbakken <oystein.bakken@nav.no>
2023-01-09 10:07:47 +0100

    Hentet VirusScanService-kode fra syfosmmottak

--- src/main/kotlin/no/nav/syfo/services/VirusScanService.kt
+++ src/main/kotlin/no/nav/syfo/services/VirusScanService.kt
@@ -12,21 +12,33 @@ class VirusScanService(
 ) {
 
     suspend fun vedleggContainsVirus(vedlegg: List<Vedlegg>): Boolean {
-        logVedleggOver300MegaByteMetric(vedlegg)
+        val vedleggOver300MegaByte = vedlegg
+            .filter { fileSizeLagerThan300MegaBytes(Base64.getMimeDecoder().decode(it.content.content)) }
 
-        log.info("Scanning vedlegg for virus, numbers of vedlegg: " + vedlegg.size)
-        val scanResultMayContainVirus = clamAvClient.virusScanVedlegg(vedlegg).filter { it.Result != Status.OK }
-        scanResultMayContainVirus.map {
-            log.warn("Vedlegg may contain virus, filename: " + it.Filename)
+        if (vedleggOver300MegaByte.isNotEmpty()) {
+            logVedleggOver300MegaByteMetric(vedleggOver300MegaByte)
         }
-        return scanResultMayContainVirus.isNotEmpty()
+
+        val vedleggUnder300MegaByte = vedlegg
+            .filter { !fileSizeLagerThan300MegaBytes(Base64.getMimeDecoder().decode(it.content.content)) }
+
+        return if (vedleggUnder300MegaByte.isEmpty()) {
+            false
+        } else {
+            log.info("Scanning vedlegg for virus, numbers of vedlegg: " + vedleggUnder300MegaByte.size)
+            val scanResultMayContainVirus = clamAvClient.virusScanVedlegg(vedleggUnder300MegaByte).filter { it.Result != Status.OK }
+            scanResultMayContainVirus.map {
+                log.warn("Vedlegg may contain virus, filename: " + it.Filename)
+            }
+            scanResultMayContainVirus.isNotEmpty()
+        }
     }
 }
 
 fun logVedleggOver300MegaByteMetric(vedlegg: List<Vedlegg>) {
     vedlegg
-        .filter { fileSizeLagerThan300MegaBytes(Base64.getMimeDecoder().decode(it.content.content)) }
-        .forEach { _ ->
+        .forEach {
+            log.info("Vedlegg is over 300 megabyte: " + it.description)
             VEDLEGG_OVER_300_MEGABYTE_COUNTER.inc()
         }
 }
