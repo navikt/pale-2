c83d79aaad62332548c0fedaba63febf45359996
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2026-03-04 20:42:00 +0100

    chore: bump to jdk 25 (#417)
    * chore: Upgrade base image to Java 25 on Debian 13
    
    * Update workflow to use plain-jar-app-25.yaml
    
    * Upgrade Java and Kotlin versions in build.gradle.kts
    
    * Update README.md
    
    * Update README.md
    
    * Refactor TestDB to use EnvironmentVariables class
    
    * Add MQ configuration parameters to TestDB
    
    * Update TestDB.kt
    
    * Update TestDB.kt
    
    * chore: clean up
    
    * chore: bumped mock version

--- src/test/kotlin/no/nav/syfo/util/TestDB.kt
+++ src/test/kotlin/no/nav/syfo/util/TestDB.kt
@@ -1,20 +1,20 @@
 package no.nav.syfo.util
 
-import io.mockk.every
-import io.mockk.mockk
+
 import no.nav.syfo.EnvironmentVariables
+import no.nav.syfo.MqUser
 import no.nav.syfo.db.Database
 import no.nav.syfo.db.DatabaseInterface
-import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.postgresql.PostgreSQLContainer
 
-class PsqlContainer : PostgreSQLContainer<PsqlContainer>("postgres:14")
+class PostgreSQLContainer : PostgreSQLContainer("postgres:14")
 
 class TestDB private constructor() {
 
     companion object {
         var database: DatabaseInterface
-        private val psqlContainer: PsqlContainer =
-            PsqlContainer()
+        private val psqlContainer: PostgreSQLContainer =
+            PostgreSQLContainer()
                 .withCommand("postgres", "-c", "wal_level=logical")
                 .withExposedPorts(5432)
                 .withUsername("username")
@@ -23,13 +23,37 @@ class TestDB private constructor() {
 
         init {
             psqlContainer.start()
-            val mockEnv = mockk<EnvironmentVariables>(relaxed = true)
-            every { mockEnv.databaseUsername } returns "username"
-            every { mockEnv.databasePassword } returns "password"
-            every { mockEnv.dbName } returns "database"
-            every { mockEnv.dbHost } returns psqlContainer.host.toString()
-            every { mockEnv.dbPort } returns psqlContainer.getMappedPort(5432).toString()
-            database = Database(mockEnv)
+            val env = EnvironmentVariables(
+                databaseUsername = psqlContainer.username,
+                databasePassword = psqlContainer.password,
+                dbHost = psqlContainer.host,
+                dbPort = psqlContainer.firstMappedPort.toString(),
+                dbName = psqlContainer.databaseName,
+                cluster = "test",
+                applicationPort = 0,
+                applicationName = "test",
+                mqHostname = "localhost",
+                mqPort = 1414,
+                mqGatewayName = "test",
+                mqChannelName = "test",
+                inputQueueName = "test",
+                apprecQueueName = "test",
+                inputBackoutQueueName = "test",
+                arenaQueueName = "test",
+                pale2ReglerApiScope = "test",
+                pdlGraphqlPath = "test",
+                pdlScope = "test",
+                aadAccessTokenV2Url = "test",
+                clientIdV2 = "test",
+                clientSecretV2 = "test",
+                paleVedleggBucketName = "test",
+                legeerklaeringBucketName = "test",
+                smgcpProxyUrl = "test",
+                smgcpProxyScope = "test",
+                smtssApiScope = "test",
+                mqUser = MqUser(username = "test", password = "test"),
+            )
+            database = Database(env)
         }
     }
 }
