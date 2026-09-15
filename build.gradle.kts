a8723582d0e8821ff2c1a286d4e0d4d5f5b35bf1
MikAojk <joakim.taule.kartveit@nav.no>
2026-09-15 08:50:20 +0200

    chore: downgrade avro

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ val testcontainerVersion="2.0.5"
 val ktfmtVersion="0.56"
 val ibmMqVersion="10.0.0.0"
 val confluentVersion="7.9.0"
-val avroVersion="1.12.2"
+val avroVersion="1.12.0"
 
 
 // Included due vulnerabilities in this transitive dependency
